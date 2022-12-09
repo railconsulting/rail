6e1ada3ce96e8985c5c4462d4056e98e94a9a465
APACIAP <103467266+APACIAP@users.noreply.github.com>
2022-12-08 21:27:31 -0600

    Update hr_payroll_payslips_by_employees.py

--- nomina_cfdi/wizard/hr_payroll_payslips_by_employees.py
+++ nomina_cfdi/wizard/hr_payroll_payslips_by_employees.py
@@ -100,7 +100,7 @@ class HrPayslipEmployeesExt(models.TransientModel):
             values = dict(default_values, **{
                 'name': _('New Payslip'),
                 'employee_id': contract.employee_id.id,
-                'credit_note': payslip_run.credit_note,
+                #'credit_note': payslip_run.credit_note,
                 'payslip_run_id': payslip_run.id,
                 'date_from': payslip_run.date_start,
                 'date_to': payslip_run.date_end,
