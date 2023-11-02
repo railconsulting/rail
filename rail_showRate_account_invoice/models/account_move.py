16b8f970fd21aec0f738f9d9ccec3da614e30dd1
ErnestoDiazDiaz <ernesto.diaz@rail.com.mx>
2023-11-02 17:17:55 -0600

    updates

--- rail_showRate_account_invoice/models/account_move.py
+++ rail_showRate_account_invoice/models/account_move.py
@@ -1,23 +1,14 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields, api
+from odoo import fields, models, api
 
-class AccountMove(models.Model):
+class AccountInvoice(models.Model):
     _inherit = 'account.move'
 
-    currency_rate_amount = fields.Float(string="Currency Rate", compute="_compute_currency_rate_amount", digits=0
-    )
-        
-    @api.depends(
-            "date",
-            "company_id",
-            "currency_id"
-    )
-    def _compute_currency_rate_amount(self):
-        #Initializing currency_rate_amount
-        self.currency_rate_amount = 1
+    discount_amount = fields.Float('Discount Amount', digits='Product Price')
+    amount_due = fields.Float(compute='_compute_amount_due', digits='Product Price', store=True)
 
-        for item in self:
-
-            rates = item.currency_id._get_rates(item.company_id, item.date)
-            item.currency_rate_amount = rates.get(item.currency_id.id)
+    @api.depends('amount_total', 'discount_amount')
+    def _compute_amount_due(self):
+        for record in self:
+            record.amount_due = record.amount_total - record.discount_amount
