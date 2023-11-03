d93ce843efd1c59d76a1b01f289a045cc429d851
ErnestoDiazDiaz <ernesto.diaz@rail.com.mx>
2023-11-02 19:44:47 -0600

    update

--- rail_showRate_account_invoice/models/account_move.py
+++ rail_showRate_account_invoice/models/account_move.py
@@ -5,4 +5,4 @@ from odoo import fields, models, api
 class AccountMove(models.Model):
     _inherit = 'account.move'
 
-    currency_rate = fields.Floats(string="Tipo de cambio")
+    x_studio_tc = fields.Floats(string="Tipo de cambio")
