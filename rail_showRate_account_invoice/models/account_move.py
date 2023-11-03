d3464d811f776b273f5e913755a5db58cdc0a9a6
ErnestoDiazDiaz <ernesto.diaz@rail.com.mx>
2023-11-03 15:53:52 -0600

    Update account_move.py

--- rail_showRate_account_invoice/models/account_move.py
+++ rail_showRate_account_invoice/models/account_move.py
@@ -40,5 +40,14 @@ class AccountMove(models.Model):
                 item.currency_rate_amount = 1/rates.get(item.currency_id.id)
             else:
                 item.currency_rate_amount = -1
+                return {
+                    'type': 'ir.actions.client',
+                    'tag': 'display_notification',
+                    'params': {
+                        'message': 'Currency rate not found',
+                        'type': 'error',
+                        'sticky': False
+                    }
+                }
 
     
