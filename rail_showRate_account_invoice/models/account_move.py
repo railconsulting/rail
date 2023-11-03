d9463208449320a00dc68af039a4923131c1dfdc
ErnestoDiazDiaz <ernesto.diaz@rail.com.mx>
2023-11-02 18:16:38 -0600

    updates

--- rail_showRate_account_invoice/models/account_move.py
+++ rail_showRate_account_invoice/models/account_move.py
@@ -2,13 +2,7 @@
 
 from odoo import fields, models, api
 
-class AccountInvoice(models.Model):
+class AccountMove(models.Model):
     _inherit = 'account.move'
 
-    discount_amount = fields.Float('Discount Amount', digits='Product Price')
-    amount_due = fields.Float(compute='_compute_amount_due', digits='Product Price', store=True)
-
-    @api.depends('amount_total', 'discount_amount')
-    def _compute_amount_due(self):
-        for record in self:
-            record.amount_due = record.amount_total - record.discount_amount
+    use_jedd = fields.Boolean(string="Prueba JEDD")
