218b9fe8b483200e13555de92d31cd86387d3ecf
ErnestoDiazDiaz <ernesto.diaz@rail.com.mx>
2023-11-03 15:34:18 -0600

    showRate_1000

--- rail_showRate_account_invoice/models/account_move.py
+++ rail_showRate_account_invoice/models/account_move.py
@@ -9,6 +9,24 @@ class AccountMove(models.Model):
         string="Tipo de cambio", compute="_compute_currency_rate_amount", digits=0,
     )
 
+    def _get_rates(self, company, date):
+        if not self.ids:
+            return {}
+        self.env['res.currency.rate'].flush_model(['rate', 'currency_id', 'company_id', 'name'])
+        query = """SELECT c.id,
+                          COALESCE((SELECT r.rate FROM res_currency_rate r
+                                  WHERE r.currency_id = c.id AND r.name = %s
+                                    AND (r.company_id IS NULL OR r.company_id = %s)
+                               ORDER BY r.company_id, r.name DESC
+                                  LIMIT 1), 1.0) AS rate
+                   FROM res_currency c
+                   WHERE c.id IN %s"""
+        self._cr.execute(query, (date, company.id, tuple(self.ids)))
+        currency_rates = dict(self._cr.fetchall())
+        return currency_rates
+
+
+
     @api.depends(
         "state",
         "date",
@@ -18,5 +36,6 @@ class AccountMove(models.Model):
     )
     def _compute_currency_rate_amount(self):
         for item in self:
-            rates = item.currency_id._get_rates(item.company_id, item.date)
+            #rates = item.currency_id._get_rates(item.company_id, item.date)
+            rates = _get_rates(item.company_id, item.date)
             item.currency_rate_amount = 1/rates.get(item.currency_id.id)
