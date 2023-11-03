ff85717f80364c1a7d373edcbd197eb27a0a3b59
ErnestoDiazDiaz <ernesto.diaz@rail.com.mx>
2023-11-03 17:54:03 -0600

    showRate_1000

--- rail_showRate_account_invoice/models/account_move.py
+++ rail_showRate_account_invoice/models/account_move.py
@@ -42,6 +42,6 @@ class AccountMove(models.Model):
                     item.currency_rate_amount = 1/rates.get(item.currency_id.id)
                 else:
                     item.currency_rate_amount = -1
-                    raise ValidationError('Currency Rate not found')
+                    raise ValidationError('Currency Rate not found', item.date)
             else:
                 item.currency_rate_amount = 1
