2f3b93b1914898d8df475419e7c3ac261af25469
ErnestoDiazDiaz <ernesto.diaz@rail.com.mx>
2023-11-02 20:53:55 -0600

    updates

--- rail_showRate_account_invoice/models/account_move.py
+++ rail_showRate_account_invoice/models/account_move.py
@@ -18,4 +18,5 @@ class AccountMove(models.Model):
     )
     def _compute_currency_rate_amount(self):
         for item in self:
-            item.currency_rate_amount = 1.23
+            rates = item.currency_id._get_rates(item.company_id, item.date)
+            item.currency_rate_amount = rates.get(item.currency_id.id)
