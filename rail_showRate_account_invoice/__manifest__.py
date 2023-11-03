26541c20d0d0b93747fb6689eb672cb6e9fb7440
ErnestoDiazDiaz <ernesto.diaz@rail.com.mx>
2023-11-02 22:27:14 -0600

    updates

--- rail_showRate_account_invoice/__manifest__.py
+++ rail_showRate_account_invoice/__manifest__.py
@@ -9,7 +9,7 @@
     # Check https://github.com/odoo/odoo/blob/16.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Customizations',
-    'version': '1.0.0.0',
+    'version': '16.0.0.2',
 
     # any module necessary for this one to work correctly
     'depends': ['account'],
