4f7b872c1c9748ec96e2a543703f248fad0660d6
ErnestoDiazDiaz <ernesto.diaz@rail.com.mx>
2023-11-02 22:19:16 -0600

    showRate_account_100

--- rail_showRate_account_invoice/__manifest__.py
+++ rail_showRate_account_invoice/__manifest__.py
@@ -1,9 +1,7 @@
 # -*- coding: utf-8 -*-
 {
-    'name': 'Rail Show Currency Rate for Invoices',
-
-    'summary': """Will look for the currency rate used in the Invoice to display it """,
-
+    'name': "Rail Show Currency Rate for Invoices",
+    'summary': "Customization to extract the rate that is used when the currency is different to the company currency and show in the transaction",
     'author': "Rail / Ernesto Diaz",
     'website': "https://rail.com.mx",
 
@@ -11,7 +9,7 @@
     # Check https://github.com/odoo/odoo/blob/16.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Customizations',
-    'version': '16.0.0.2',
+    'version': '1.0.0.0',
 
     # any module necessary for this one to work correctly
     'depends': ['account'],
