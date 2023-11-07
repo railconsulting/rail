fc6a2a023045034f484db2de84dc73a8eefcce1f
ErnestoDiazDiaz <ernesto.diaz@rail.com.mx>
2023-11-07 09:33:20 -0600

    showRate_1000

--- rail_showRate_account_invoice/models/purchase_order.py
+++ rail_showRate_account_invoice/models/purchase_order.py
@@ -8,14 +8,3 @@ class PurchaseOrder(models.Model):
     currency_rate_amount = fields.Float(
         string="Tipo de cambio", compute="_compute_currency_rate_amount", digits=0,
     )
-
-    @api.depends(
-        "date",
-        "company_id",
-        "currency_id"
-    )
-    def _compute_currency_rate_amount(self):
-        currencyRate=0
-        
-        for item in self:
-            item.currency_rate_amount = 10.1234
