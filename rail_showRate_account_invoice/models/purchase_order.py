05f5d4db0bf225f0612cd19ff9215d45bb81c3cd
ErnestoDiazDiaz <ernesto.diaz@rail.com.mx>
2023-11-07 10:46:30 -0600

    showRate_1000

--- rail_showRate_account_invoice/models/purchase_order.py
+++ rail_showRate_account_invoice/models/purchase_order.py
@@ -10,6 +10,20 @@ class PurchaseOrder(models.Model):
         string="Tipo de cambio", compute="_compute_currency_rate_amount", digits=0,
     )
 
+    def get_rates(self, currency, company, date):
+        self.env['res.currency.rate'].flush_model(['rate', 'currency_id', 'company_id', 'name'])
+        query = """SELECT c.id,
+                          COALESCE((SELECT r.rate FROM res_currency_rate r
+                                  WHERE r.currency_id = c.id AND r.name = %s
+                                    AND (r.company_id IS NULL OR r.company_id = %s)
+                               ORDER BY r.company_id, r.name DESC
+                                  LIMIT 1), 1.0) AS rate
+                   FROM res_currency c
+                   WHERE c.id IN (%s)"""
+        self._cr.execute(query, (date, company.id, currency.id))
+        currency_rates = dict(self._cr.fetchall())
+        return currency_rates
+    
     @api.depends(
         "date_order",
         "company_id",
