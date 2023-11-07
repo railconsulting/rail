87dbc03d74898d7bdaa11b2d1a0524042147ae74
ErnestoDiazDiaz <ernesto.diaz@rail.com.mx>
2023-11-07 12:27:03 -0600

    showRate_1000

--- rail_showRate_account_invoice/models/sale_order.py
+++ rail_showRate_account_invoice/models/sale_order.py
@@ -9,3 +9,37 @@ class SaleOrder(models.Model):
     currency_rate_amount = fields.Float(
         string="Tipo de cambio", compute="_compute_currency_rate_amount", digits=0,
     )
+
+    def get_rates(self, currency, company, date):
+        self.env['res.currency.rate'].flush_model(['rate', 'currency_id', 'company_id', 'name'])
+        query = """SELECT c.id,
+                          COALESCE((SELECT r.rate FROM res_currency_rate r
+                                  WHERE r.currency_id = c.id AND r.name = %s
+                                    AND (r.company_id IS NULL OR r.company_id = %s)
+                               ORDER BY r.company_id, r.name DESC
+                                  LIMIT 1), 1.0) AS rate
+                   FROM res_currency c
+                   WHERE c.id IN (%s)"""
+        self._cr.execute(query, (date, company.id, currency.id))
+        currency_rates = dict(self._cr.fetchall())
+        return currency_rates
+    
+    @api.depends(
+        "date_order",
+        "company_id",
+        "currency_id"
+    )
+    def _compute_currency_rate_amount(self):
+        currencyRate=0
+        
+        for item in self:
+            if item.currency_id != item.company_id.currency_id:
+                rates = self.get_rates(item.currency_id, item.company_id, item.date_order)
+                currencyRate = rates.get(item.currency_id.id)
+                if currencyRate == 1.0:
+                    item.currency_rate_amount = -1
+                    raise ValidationError('Currency rate not found for date ' + str(item.date_order))
+                else:
+                    item.currency_rate_amount = 1/currencyRate
+            else:
+                item.currency_rate_amount = 1
