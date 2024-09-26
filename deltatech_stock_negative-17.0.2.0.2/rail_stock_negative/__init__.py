0e8f87b413e5849a85a0628894d202d0681943ac
FranciscoRodriguezBotello <105890754+FranciscoRodriguezBotello@users.noreply.github.com>
2024-09-26 15:52:32 -0600

    Stock Control

---
+++ deltatech_stock_negative-17.0.2.0.2/rail_stock_negative/__init__.py
@@ -0,0 +1,5 @@
+# ©  2008-2021 Rail Consulting
+# See README.rst file on addons root folder for license details
+
+
+from . import models
---
+++ deltatech_stock_negative-17.0.2.0.2/rail_stock_negative/tests/__init__.py
@@ -0,0 +1,4 @@
+# ©  2008-2021 Rail Consulting
+# See README.rst file on addons root folder for license details
+
+from . import test_negative
