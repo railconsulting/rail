e22fb690361b24fb14792591f35cc27fd752c388
FranciscoRodriguezBotello <105890754+FranciscoRodriguezBotello@users.noreply.github.com>
2024-09-26 16:25:44 -0600

    Negative Stock Control

---
+++ deltatech_stock_negative-17.0.2.0.2/deltatech_stock_negative/__manifest__.py
@@ -0,0 +1,18 @@
+# ©  2008-2021 Rail Consulting
+# See README.rst file on addons root folder for license details
+
+{
+    "name": "Rail - No Negative Stock",
+    "summary": "No permite transacciones con stock en negativo",
+    "version": "17.0.2.0.2",
+    "author": "Rail Consulting, Dorin Hongu",
+    "website": "https://www.terrabit.ro",
+    "category": "Generic Modules/Stock",
+    "depends": ["stock"],
+    "license": "OPL-1",
+    "data": ["views/res_config_view.xml", "views/stock_location_view.xml"],
+    "images": ["static/description/main_screenshot.png"],
+    "installable": True,
+    "development_status": "Mature",
+    "maintainers": ["dhongu"],
+}
